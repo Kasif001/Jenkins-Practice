71e614c62c0d08573877b2ac37ca43b45a9f617e
Your Name <kasifaliplk07860@gmail.com>
2024-04-21 20:47:27 +0530

    Jenkins 1

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+        System.out.println("This is java program");
+        System.out.println("ouput form java program");
+        System.out.println(new Date());
+	}
+}
